e7fc5fc779cf753908d9f8c52bc6913e8a5370ce
alebou21 <adrien.lebourgeois@protonmail.com>
2018-04-04 17:00:58 +0200

    nouveau fichier : sql/bdd.sql

---
+++ sql/bdd.sql
@@ -0,0 +1,40 @@
+CREATE DATABASE NACA;
+USE NACA;
+
+DROP TABLE IF EXISTS parametre;
+DROP TABLE IF EXISTS cambrure;
+
+CREATE TABLE parametre{
+    id INT NOT NULL auto_increment,
+    libelle VARCHAR(128),
+    corde FLOAT NOT NULL,
+    tmaxmm FLOAT NOT NULL,
+    tmaxpercent FLOAT NOT NULL,
+    fmaxmm FLOAT NOT NULL,
+    fmaxpercent FLOAT NOT NULL,
+    nb_points INT NOT NULL,
+    date DATETIME,
+    fic_img VARCHAR(128),
+    fic_csv VARCHAR(128),
+
+    PRIMARY KEY(id)
+}
+engine = innodb;
+
+CREATE TABLE cambrure{
+    id INT NOT NULL auto_increment,
+    x FLOAT NOT NULL,
+    t FLOAT NOT NULL,
+    f FLOAT NOT NULL,
+    yintra FLOAT NOT NULL,
+    yextra FLOAT NOT NULL,
+    id_param INT NOT NULL,
+    igx FLOAT NOT NULL,
+
+    PRIMARY KEY(id),
+    FOREIGN KEY(id_param) REFERENCES parametre(id)
+}
+engine = innodb;
+
+set autocommit = 0;
+set names utf8;
