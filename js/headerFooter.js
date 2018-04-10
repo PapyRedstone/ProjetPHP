84a24f58358f82ae1daf202c2e2cb9fedfe8f699
alebou21 <adrien.lebourgeois@protonmail.com>
2018-04-10 19:09:42 +0200

    i

--- js/headerFooter.js
+++ js/headerFooter.js
@@ -6,14 +6,7 @@ var header = document.createElement("header");
 var body = document.getElementsByTagName("body");
 var navBar;
 
-navBar =document.createElement('<div class="navbar navbar-default">'+
-                                    '<ul class="nav navbar-nav">'+
-                                    '<li class="active"> <a href="#">Accueil</a> </li>'+
-                                    '<li> <a href="#">Liens</a> </li>'+
-                                    '<li> <a href="#">Témoignages</a> </li>'+
-                                    '<li class="disabled"> <a href="#">Références</a> </li>'+
-                                    '</ul>'+
-                                '</div>');
+navBar =document.createElement('<div class="navbar navbar-default">'+'<ul class="nav navbar-nav">'+'<li class="active"> <a href="#">Accueil</a> </li>'+'<li> <a href="#">Liens</a> </li>'+'<li> <a href="#">Témoignages</a> </li>'+'<li class="disabled"> <a href="#">Références</a> </li>'+'</ul>'+'</div>');
 
 header.appendChild(navBar);
 body.insertBefore(header, body.childNodes[0]);
