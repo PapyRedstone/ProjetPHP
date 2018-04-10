612c412cc58cc5ce0925f9872ff4c09ff5fb826c
alebou21 <adrien.lebourgeois@protonmail.com>
2018-04-10 14:50:07 +0200

    commençant par '#' seront ignorées, et un message vide abandonne la validation.

--- php/Parametres.php
+++ php/Parametres.php
@@ -16,6 +16,52 @@ class Parametres{
     {
         return $this->id, $this->libelle, $this->corde, $this->tMaxPercent, $this->tMaxmm, $this->fMaxPercent, $this->fMaxmm, $this->nbPoints, $this->date;
     }
+
+    public function getId(){
+
+        return $this->id;
+    }
+
+    public function getLibelle(){
+
+        return $this->libelle;
+    }
+
+    public function getCorde(){
+
+        return $this->corde;
+    }
+
+    public function getTMaxPercent(){
+
+        return $this->tMaxPercent;
+    }
+
+    public function getTMaxmm(){
+
+        return $this->tMaxmm;
+    }
+
+    public function getFMaxPercent(){
+
+        return $this->fMaxPercent;
+    }
+
+    public function getFMaxmm(){
+
+        return $this->fMaxmm;
+    }
+
+    public function getNbPoints(){
+
+        return $this->nbPoints;
+    }
+
+    public function getDate(){
+
+        return $this->date;
+    }
+
 }
 
 ?>
