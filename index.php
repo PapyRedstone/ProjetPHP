575873b20730996edc5e346733c8f80c1397394e
alebou21 <adrien.lebourgeois@protonmail.com>
2018-04-10 17:23:11 +0200

    1

--- index.php
+++ index.php
@@ -1,4 +1,18 @@
 <html>
+  <head>
+    		<!-- Meta tags -->
+		<meta charset="utf-8" />
+		<meta name="author" content="Febvre_Lebourgeois" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+		<!-- Title -->
+		<title>Nacalculator</title>
+
+		<!-- Js Scripts -->
+		<script src="js/bootstrap.min.js" defer></script>
+		<script type="text/javascript" src="js/headerFooter.js" defer></script>
+
+  </head>
   <body>
 
     <?php
@@ -10,5 +24,6 @@
       $n = new Naca($db,1);
     ?>
   
+
   </body>
 </html>
---
+++ js/headerFooter.js
@@ -0,0 +1,21 @@
+'use strict';
+
+//var divs = document.getElementsByTagName("body");
+
+var header = document.createElement("header");
+var body = document.getElementsByTagName("body");
+var navBar;
+
+navBar = '<nav class="navbar navbar-inverse">'+
+            '<div class="container-fluid">'+
+                '<ul class="nav navbar-nav">'+
+                    '<li class="active"> <a href="#">Accueil</a> </li>'+
+                    '<li> <a href="#">Liens</a> </li>'+
+                    '<li> <a href="#">Témoignages</a> </li>'+
+                    '<li> <a href="#">Références</a> </li>'+
+                '</ul>'+
+            '</div>'+
+        '</nav>';
+
+header.appendChild(navBar);
+body.insertBefore(header, body.childNodes[0]);
