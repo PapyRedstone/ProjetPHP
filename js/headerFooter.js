28d9261b7d6bc2c9eff53415b9a5b498383f90c5
alebou21 <adrien.lebourgeois@protonmail.com>
2018-04-10 18:42:58 +0200

    a

--- js/headerFooter.js
+++ js/headerFooter.js
@@ -6,15 +6,15 @@ var header = document.createElement("header");
 var body = document.getElementsByTagName("body");
 var navBar;
 
-navBar = '<nav class="navbar navbar-inverse">'+
-            '<div class="container-fluid">'+
+navBar = '<nav class="navbar navbar-default">'+
+            /*'<div class="container-fluid">'+*/
                 '<ul class="nav navbar-nav">'+
                     '<li class="active"> <a href="#">Accueil</a> </li>'+
                     '<li> <a href="#">Liens</a> </li>'+
                     '<li> <a href="#">Témoignages</a> </li>'+
                     '<li> <a href="#">Références</a> </li>'+
                 '</ul>'+
-            '</div>'+
+            /*'</div>'+*/
         '</nav>';
 
 //header = navBar.innerHTML;
