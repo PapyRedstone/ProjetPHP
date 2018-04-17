282eff5030e666af5a4c3d933bbb676dbce2f7fb
PapyRedstone <febvrealex@yahoo.fr>
2018-04-17 13:32:20 +0200

    modifié :         index.php 	modifié :         php/Naca.php

--- index.php
+++ index.php
@@ -2,13 +2,13 @@
   <body>
 
     <?php
-      require 'php/database.php';
-      require "php/Naca.php";
+require 'php/database.php';
+require "php/Naca.php";
 
-      $db = new Database();
+$db = new Database();
 
-      $n = new Naca($db,1);
-    ?>
-  
+$n = new Naca($db,2);
+?>
+
   </body>
 </html>
--- php/Naca.php
+++ php/Naca.php
@@ -10,15 +10,40 @@ class Naca{
   private $db;
 
   function __construct($db,$id){
-
     $this->db = $db;
-    $this->parametre = $this->db->execute("SELECT * FROM parametre WHERE id = $id",null,"Parametres")[0];
+    $r = $this->db->execute("SELECT * FROM parametre WHERE id = $id",null,"Parametres");
+    if(!isset($r[0])){
+      return;
+    }
+    $this->parametre = $r[0];
     echo $this->parametre."<br>";
     $this->cambrures = $this->db->execute("SELECT * FROM cambrure WHERE idParam = $id",null,"Cambrure");
-    /*echo "<pre>";
-    var_dump($this->cambrures);
-    echo "</pre>";*/
-    echo $this->cambrures[0];
+    if(!isset($this->cambrures[0])){
+      $this->calculateCambrure($this->parametre->getId(),$this->parametre->getLibelle(),$this->parametre->getCorde(),$this->parametre->getTMaxmm(),$this->parametre->getFMaxmm(),$this->parametre->getNbPoints());
+    }
+  }
+
+  function getT($X,$tmmm){
+    return -(1.015*pow($X,4)-2.843*pow($X,3)+3.516*pow($X,2)+1.26*$X-2.969*sqrt($X))*$tmmm;
+  }
+
+  function calculateCambrure($id,$lib,$c,$tmmm,$fmmm,$nb_points){
+    $dX = $c/$nb_points;
+    $sommedS = 0;
+    $sommedXdS = 0;
+    for($x=0;$x<=$c;$x+=$dX){
+      $X = $x/$c;
+      $t = $this->getT($X,$tmmm);
+      $f = -4*(pow($X,2)-$X)*$fmmm;
+      $yI = f-t/2;
+      $yE = f+t/2;
+
+      $dS = dX*getT($X+$dX/2);
+
+      $sommedS += dS;
+      $sommedXdS += dS * $X+$dX/2;
+    }
+    $Xg = $sommedXdS / $sommedS;
   }
 }
 ?>
